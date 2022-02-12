b7f4af8e26c0c2199536a00840b4e9406a81d969
hoshmandeep <78502348+hoshmandeep@users.noreply.github.com>
2022-02-11 22:18:15 -0500

    final lab1

--- DiceGame.java
+++ DiceGame.java
@@ -1,7 +1,7 @@
 public class DiceGame {
 
     public static void main(String[] args) {
-
+// In each dice, it shows which side is up first.
 System.out.println("Creating a default d6...");
         Die d6 = new Die();
 
@@ -11,27 +11,32 @@ System.out.println("Creating a default d20...");
 System.out.println("reating percentile die (a special d10)...");
         Die d10 = new Die(10);
 
+
 System.out.println("The current side up for " + d6.getName() + " is " + d6.getSideUp());
 
 System.out.println("The current side up for " + d20.getName() + " is " + d20.getSideUp());
 
 System.out.println("The current side up for Percentile  is " + d10.getSideUp());
 
+//procedure of a test roll
 System.out.println("\nTesting the roll method");
+
+//To get new value, roll the dice d6
 System.out.println("Rolling the d6...");
         d6.roll();
-
 System.out.println("The new value is " + d6.getSideUp());
+
+// To get new value, role d20
 System.out.println("Rolling the d20...");
         d20.roll();
-
 System.out.println("The new value is " + d20.getSideUp());
 
+//To get new value, role d10
 System.out.println("Rolling the Percentile...");
         d10.roll();
-
 System.out.println("The new value is " + d10.getSideUp());
 
+//final last step
 System.out.println("\nSetting the d20 to show 20...");
         d20.setSideUp(20);
 
--- Die.java
+++ Die.java
@@ -1,25 +1,31 @@
+//Name: Hoshmandeep kaur
+//Student number: A00227084
 public class Die {
 
+// Three variables
   private String Dicename;
   private int Dicesides;
   private int DicesideUp;
 
+// Die class is used
+//0 argument
     public Die() {
         Dicename = "d6";
         Dicesides = 6;
-        roll();
+        roll(); // role method is used
 
     }
-
+// die constructer with 1 argument
   public Die(int Dicesides) {
         this.Dicesides = Dicesides;
         Dicename = "d" + Dicesides;
-        roll();
+        roll(); // Role method is used
     }
 
+// die constructer with 2 argumen
   public Die(int Dicesides, int DicesideUp) {
         this.Dicesides = Dicesides;
-        Dicename = "d" + sides;
+        Dicename = "d" + Dicesides;
         this.DicesideUp = DicesideUp;
     }
 
@@ -27,22 +33,24 @@ public class Die {
   public void roll() {
         DicesideUp = (int) (1 + Math.random() * Dicesides);
     }
-
+// I used this for Dicename
   public String getName() {
         return Dicename;
     }
 
+// I used this for Dicesides
 public int getSides() {
-        return Dicesides;
-    }
+  return Dicesides;
+}
 
+// I used this for Dicesideup
 public int getSideUp() {
         return DicesideUp;
     }
 
     public void setSides(int Dicesides) {
         this.Dicesides = Dicesides;
-        Dicename = "d6";
+        Dicename = "d6";// This is number of sides
     }
 
     public void setSideUp(int sideUp) {
